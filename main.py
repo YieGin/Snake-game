9e41c1d3f35479b7a1f1c4b1fb67cae5c777002c
YieGin <islampuma50@gmail.com>
2023-11-10 02:37:38 +0100

    snake game

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("snake game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    
+    #Detect collistion with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        
+    #Detect collision with wall.
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        is_game_on = False
+        scoreboard.game_over()
+    #Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+    #if head collides with any segment in the tail:
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+ALIGN = "center"
+FONT = ("Arial", 20, "normal")
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.update_score()
+        self.hideturtle()
+    def update_score(self):
+        self.write(f"score: {self.score}", align=ALIGN, font=FONT)
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game over", align="center", font=("Arial", 20, "normal"))
+        
